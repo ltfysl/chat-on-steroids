a386ee48e801452489410dddc5cb278eb21718e0
ltfysl <33989040+ltfysl@users.noreply.github.com>
2026-09-09 03:25:15 -0700

    feat(grokbot): connect persistent chat routes

--- apps/grokbot/routes/web.php
+++ apps/grokbot/routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BotController;
 use App\Http\Controllers\BotDeploymentController;
+use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function (): void {
@@ -21,4 +23,7 @@ Route::middleware('auth')->group(function (): void {
     Route::resource('bots', BotController::class)->except(['edit']);
     Route::post('/bots/{bot}/deploy', [BotDeploymentController::class, 'store'])->name('bots.deploy');
     Route::delete('/bots/{bot}/deployment', [BotDeploymentController::class, 'destroy'])->name('bots.stop');
+    Route::post('/bots/{bot}/conversations', [ConversationController::class, 'store'])->name('bots.conversations.store');
+    Route::get('/conversations/{conversation}', [ConversationController::class, 'show'])->name('conversations.show');
+    Route::post('/conversations/{conversation}/messages', [MessageController::class, 'store'])->middleware('throttle:bot-messages')->name('messages.store');
 });
